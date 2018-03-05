39128ac2a5d0e0322d544e9c21e96056839883a1
sansaoui <yassine.sansaoui@gmail.com>
2018-03-05 16:51:16 +0100

    add transitions activities

--- app/src/main/java/project/meapy/meapy/CreateGroupActivity.java
+++ app/src/main/java/project/meapy/meapy/CreateGroupActivity.java
@@ -1,13 +1,30 @@
 package project.meapy.meapy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import project.meapy.meapy.groups.joined.MyGroupsActivity;
 
 public class CreateGroupActivity extends AppCompatActivity {
+
+    private Button createNewGroupId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+
+        createNewGroupId = (Button)findViewById(R.id.createNewGroupId);
+
+        createNewGroupId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateGroupActivity.this, MyGroupsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/project/meapy/meapy/groups/joined/MyGroupsActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/joined/MyGroupsActivity.java
@@ -3,13 +3,17 @@ package project.meapy.meapy.groups.joined;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import project.meapy.meapy.CreateGroupActivity;
 import project.meapy.meapy.R;
 import project.meapy.meapy.SendFileActivity;
+import project.meapy.meapy.groups.discussions.DiscussionGroupsActivity;
 
 /**
  * Created by yassi on 23/02/2018.
@@ -17,10 +21,22 @@ import project.meapy.meapy.SendFileActivity;
 
 public class MyGroupsActivity extends AppCompatActivity {
 
+    private FloatingActionButton createGroupId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_groups);
+
+        createGroupId = (FloatingActionButton)findViewById(R.id.createGroupId);
+
+        createGroupId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyGroupsActivity.this, CreateGroupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,7 +55,7 @@ public class MyGroupsActivity extends AppCompatActivity {
                 break;
 
             case R.id.discussionsId:
-
+                intent = new Intent(this, DiscussionGroupsActivity.class);
                 break;
         }
         startActivity(intent);
