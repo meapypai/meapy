55bade01fd89adb6a7fb70ed271e83ab07f4fffe
sansaoui <yassine.sansaoui@gmail.com>
2018-03-05 16:07:54 +0100

    create link menu

--- app/src/main/java/project/meapy/meapy/groups/joined/MyGroupsActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/joined/MyGroupsActivity.java
@@ -1,27 +1,48 @@
 package project.meapy.meapy.groups.joined;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import project.meapy.meapy.R;
+import project.meapy.meapy.SendFileActivity;
 
 /**
  * Created by yassi on 23/02/2018.
  */
 
-public class MyGroupsActivity extends Activity {
+public class MyGroupsActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_groups);
     }
 
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu_my_groups, menu);
-//        return true;
-//    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_my_groups, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent intent = null ;
+        switch(item.getItemId()) {
+            case R.id.addFileId:
+                intent = new Intent(this, SendFileActivity.class);
+                break;
+
+            case R.id.discussionsId:
+
+                break;
+        }
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
 }
