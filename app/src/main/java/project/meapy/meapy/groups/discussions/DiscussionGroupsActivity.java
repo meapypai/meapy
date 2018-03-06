b6149488c322df51b1f477d50213de7c176ce4f5
assimtsenoussi <assim.senoussi@gmail.com>
2018-03-06 16:58:30 +0100

    changing theme of chatroom in manifest, adding link to chatroom from discussion group activity, modification of chat room activity

--- app/src/main/java/project/meapy/meapy/groups/discussions/DiscussionGroupsActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/discussions/DiscussionGroupsActivity.java
@@ -1,13 +1,17 @@
 package project.meapy.meapy.groups.discussions;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import project.meapy.meapy.ChatRoomActivity;
 import project.meapy.meapy.R;
 import project.meapy.meapy.groups.Group;
 import project.meapy.meapy.groups.GroupAdapter;
@@ -33,8 +37,14 @@ public class DiscussionGroupsActivity extends AppCompatActivity {
         groups.add(new Group(R.drawable.maths,"Groupe 2","Il y a des erreurs"));
         groups.add(new Group(R.drawable.web,"Groupe 3","merci"));
 
-        GroupAdapter adapter = new GroupAdapter(this,android.R.layout.simple_expandable_list_item_2,groups);
+        final GroupAdapter adapter = new GroupAdapter(this,android.R.layout.simple_expandable_list_item_2,groups);
 
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                startActivity(new Intent(DiscussionGroupsActivity.this, ChatRoomActivity.class));
+            }
+        });
     }
 }
