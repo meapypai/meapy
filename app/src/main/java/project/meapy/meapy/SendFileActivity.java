8c924465fe5878677513b110298a16c3cb557a00
sansaoui <yassine.sansaoui@gmail.com>
2018-03-07 01:12:48 +0100

    add permission into send file activity to read external storage

--- app/src/main/java/project/meapy/meapy/SendFileActivity.java
+++ app/src/main/java/project/meapy/meapy/SendFileActivity.java
@@ -1,8 +1,12 @@
 package project.meapy.meapy;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +21,7 @@ import project.meapy.meapy.utils.ProviderFilePath;
 
 public class SendFileActivity extends AppCompatActivity {
 
+    private static  final int REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 7;
     private static final int REQUEST_LOAD_FILE = 5;
 
     private Button importFileBtnSend;
@@ -36,6 +41,13 @@ public class SendFileActivity extends AppCompatActivity {
         //edittext
         fileNameSend      = (EditText) findViewById(R.id.fileNameSend);
 
+
+        //permission
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
+        }
+
+        //listeners
         importFileBtnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
