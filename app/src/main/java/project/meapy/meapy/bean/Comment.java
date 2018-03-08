90ba3da9f84f730b28c8ee28e95f0b800017809e
assimtsenoussi <assim.senoussi@gmail.com>
2018-03-08 19:30:52 +0100

    adding comment object

--- app/src/main/java/project/meapy/meapy/bean/Comment.java
+++ app/src/main/java/project/meapy/meapy/bean/Comment.java
@@ -1,8 +1,37 @@
 package project.meapy.meapy.bean;
 
+import java.sql.Timestamp;
+
 /**
  * Created by tarek on 08/03/18.
  */
 
 public class Comment extends DomainObject {
+    private int postId;
+    private int userId;
+    private Timestamp date;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
 }
