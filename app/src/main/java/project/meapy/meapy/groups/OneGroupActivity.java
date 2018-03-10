4a81e8574b9c28b2881754bfe8567cf9b6364561
assimtsenoussi <assim.senoussi@gmail.com>
2018-03-10 14:59:05 +0100

    adding add matiere button into one group actiivty

--- app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
@@ -17,7 +17,11 @@ public class OneGroupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_one_group);
         Groups grp = (Groups) getIntent().getSerializableExtra("GROUP");
         TextView titleGroup = findViewById(R.id.groupNameOneGroup);
-        titleGroup.setText(grp.toString());
+        String name = grp.getName();
+        int idAdmin = grp.getIdUserAdmin();
+        int idGrp = grp.getId();
+        int limituser = grp.getLimitUsers();
+        titleGroup.setText(String.format("group name :%s\nid group :%d\nid admin :%d\nlimit user :%d",name,idGrp,idAdmin,limituser));
 
     }
 }
