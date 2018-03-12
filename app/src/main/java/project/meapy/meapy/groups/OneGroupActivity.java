419f155d1c47310aa3ca5077f9a008b759c4fbb1
Senoussi Assim-tarek <senoussi@a10p16.fil.univ-lille1.fr>
2018-03-12 15:09:55 +0100

    adding sidebar into one group view

--- app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
@@ -2,12 +2,17 @@ package project.meapy.meapy.groups;
 
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +21,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import project.meapy.meapy.R;
 import project.meapy.meapy.SendFileActivity;
@@ -28,6 +36,12 @@ public class OneGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_group);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_one_group);
+        drawer.openDrawer(GravityCompat.START);
+
+
+
         Groups grp = (Groups) getIntent().getSerializableExtra("GROUP");
         TextView titleGroup = findViewById(R.id.groupNameOneGroup);
         String name = grp.getName();
@@ -37,7 +51,9 @@ public class OneGroupActivity extends AppCompatActivity {
         titleGroup.setText(String.format("group name :%s\nid group :%d\nid admin :%d\nlimit user :%d",name,idGrp,idAdmin,limituser));
 
         final GridView grid = findViewById(R.id.postsOneGroup);
-
+        List<String> list = new ArrayList<String>();
+        list.add("1");list.add("2");list.add("3");list.add("4");
+        grid.setAdapter(new ArrayAdapter<String>(OneGroupActivity.this, android.R.layout.simple_expandable_list_item_1,list));
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference postsRef = database.getReference("groups/"+grp.getId()+"/posts/");
         postsRef.addChildEventListener(new ChildEventListener() {
