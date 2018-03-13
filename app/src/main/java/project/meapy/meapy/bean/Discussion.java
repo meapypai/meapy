8543297bf6059383d6aa33c6975c68da84ae3f4b
sansaoui <yassine.sansaoui@gmail.com>
2018-03-13 17:54:19 +0100

    create class Discussion (DomainObject)

---
+++ app/src/main/java/project/meapy/meapy/bean/Discussion.java
@@ -0,0 +1,12 @@
+package project.meapy.meapy.bean;
+
+/**
+ * Created by yassi on 13/03/2018.
+ */
+
+public class Discussion extends DomainObject {
+
+    public Discussion() {
+        super();
+    }
+}
