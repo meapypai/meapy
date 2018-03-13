a094a6330d4a1b0b8640bd3e6d3f7abc4dfaf4b7
assimtsenoussi <assim.senoussi@gmail.com>
2018-03-13 18:54:55 +0100

    reorganize navigtion view on one groups view

--- app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
@@ -5,12 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -44,7 +50,7 @@ public class OneGroupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_one_group);
         final Groups grp = (Groups) getIntent().getSerializableExtra("GROUP");
 
-        // A REVOIR
+        // A REVOIR ( mettre une toolbar)
         ImageButton openDrawer = findViewById(R.id.openDrawerBtn);
         openDrawer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,22 +59,41 @@ public class OneGroupActivity extends AppCompatActivity {
                 drawer.openDrawer(GravityCompat.START);
             }
         });
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_oneGroup);
+        //setSupportActionBar(myToolbar);
+       /* getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);*/
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, (DrawerLayout) findViewById(R.id.drawer_one_group),myToolbar,R.string.app_name,R.string.app_name);
+        //DrawerLayout drawerLayout = ((DrawerLayout) findViewById(R.id.drawer_one_group));
+
+        //toggle = new ActionBarDrawerToggle(this,drawerLayout,0,0);
+        //drawerLayout.setDrawerListener(toggle);
         //FIN A REVOIR
+        Menu menu = ((NavigationView)findViewById(R.id.side_menu_one_group)).getMenu();
+        final SubMenu subMenuDisc = menu.addSubMenu("Discipline");
+        Button addDisc = findViewById(R.id.addDiscOneGroup);
+        addDisc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(OneGroupActivity.this,"add groups",Toast.LENGTH_LONG).show();
+                DrawerLayout drawer = findViewById(R.id.drawer_one_group);
+                drawer.closeDrawer(GravityCompat.START);
+                Intent intent = new Intent(OneGroupActivity.this, AddDisciplineActivity.class);
+                intent.putExtra("GROUP",grp);
+                startActivity(intent);
+            }
+        });
+
+
         final NavigationView navigationView = findViewById(R.id.side_menu_one_group);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
                         menuItem.setChecked(true);
-                        if(menuItem.getItemId() == R.id.addDiscOneGroup){
-                            Toast.makeText(OneGroupActivity.this,"add groups",Toast.LENGTH_LONG).show();
-                            DrawerLayout drawer = findViewById(R.id.drawer_one_group);
-                            drawer.closeDrawer(GravityCompat.START);
-                            Intent intent = new Intent(OneGroupActivity.this, AddDisciplineActivity.class);
-                            intent.putExtra("GROUP",grp);
-                            startActivity(intent);
-                        }
+                        Toast.makeText(getApplicationContext(),"click on disc",Toast.LENGTH_LONG).show();
+                        // recupere si c'est une discipline et lance la bonne activity
+                        menuItem.setChecked(false);
                         return true;
                     }
                 });
@@ -85,8 +110,8 @@ public class OneGroupActivity extends AppCompatActivity {
 
         final ListView listView = findViewById(R.id.postsOneGroup);
         List<Post> list = new ArrayList<Post>();
-        final ArrayAdapter adapter = new ArrayAdapter<Post>(OneGroupActivity.this, android.R.layout.simple_expandable_list_item_1,list);
-        listView.setAdapter(adapter);
+        final ArrayAdapter adapterPost = new ArrayAdapter<Post>(OneGroupActivity.this, android.R.layout.simple_expandable_list_item_1,list);
+        listView.setAdapter(adapterPost);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference postsRef = database.getReference("groups/"+grp.getId()+"/disciplines/");
@@ -94,11 +119,12 @@ public class OneGroupActivity extends AppCompatActivity {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Discipline disc = dataSnapshot.getValue(Discipline.class);
+                subMenuDisc.add(disc.getName());
                 dataSnapshot.getRef().child("posts").addChildEventListener(new ChildEventListener() {
                     @Override
                     public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                         Post post = dataSnapshot.getValue(Post.class);
-                        adapter.add(post);
+                        adapterPost.add(post);
                     }
 
                     @Override
@@ -147,7 +173,7 @@ public class OneGroupActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Post post = (Post) adapter.getItem(i);
+                Post post = (Post) adapterPost.getItem(i);
                 Intent intent = new Intent(OneGroupActivity.this, PostDetailsActivity.class);
                 intent.putExtra("POST",post);
                 startActivity(intent);
