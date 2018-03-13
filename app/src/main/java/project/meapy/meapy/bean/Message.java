ab883d0abc0d3b7671ab9a0bef8bd7e98c8f0800
sansaoui <yassine.sansaoui@gmail.com>
2018-03-13 02:04:17 +0100

    construction adapter in discussion  group

---
+++ app/src/main/java/project/meapy/meapy/bean/Message.java
@@ -0,0 +1,11 @@
+package project.meapy.meapy.bean;
+
+/**
+ * Created by yassi on 13/03/2018.
+ */
+
+public class Message extends DomainObject {
+
+    private String content;
+    private String user;
+}
--- app/src/main/java/project/meapy/meapy/groups/discussions/DiscussionGroupsActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/discussions/DiscussionGroupsActivity.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import project.meapy.meapy.ChatRoomActivity;
 import project.meapy.meapy.R;
+import project.meapy.meapy.bean.Groups;
 import project.meapy.meapy.groups.DiscussionGroup;
 import project.meapy.meapy.groups.DiscussionGroupAdapter;
 
@@ -22,6 +31,8 @@ import project.meapy.meapy.groups.DiscussionGroupAdapter;
 public class DiscussionGroupsActivity extends AppCompatActivity {
 
     private ListView listView;
+    private FirebaseDatabase database;
+    private FirebaseListAdapter<Groups> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +43,21 @@ public class DiscussionGroupsActivity extends AppCompatActivity {
         listView = (ListView)findViewById(R.id.listDiscussionGroups);
 
         List<DiscussionGroup> groups = new ArrayList<>();
-        groups.add(new DiscussionGroup(R.drawable.bdd,"Groupe 1","Je ne comprends pas...",null));
-        groups.add(new DiscussionGroup(R.drawable.maths,"Groupe 2","Il y a des erreurs",null));
-        groups.add(new DiscussionGroup(R.drawable.web,"Groupe 3","merci",null));
 
-        final DiscussionGroupAdapter adapter = new DiscussionGroupAdapter(this,android.R.layout.simple_expandable_list_item_2,groups);
+        adapter = new FirebaseListAdapter<Groups>(this,Groups.class, R.layout.group_view,FirebaseDatabase.getInstance().getReference("groups")) {
+            @Override
+            protected void populateView(View v, Groups model, int position) {
+                ImageView image = (ImageView)v.findViewById(R.id.imgGroup);
+                TextView name = (TextView)v.findViewById(R.id.nameGroup);
+                TextView message = (TextView)v.findViewById(R.id.lastMessage);
+
+                name.setText(model.getName());
+                message.setText(model.getIdUserAdmin()+"");
+            }
+        };
 
         listView.setAdapter(adapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
