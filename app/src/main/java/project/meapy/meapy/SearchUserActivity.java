071610ac554906d235520a9fd2724aead17fae89
sansaoui <yassine.sansaoui@gmail.com>
2018-03-16 14:25:01 +0100

    add SearchUserActivity

---
+++ app/src/main/java/project/meapy/meapy/SearchUserActivity.java
@@ -0,0 +1,23 @@
+package project.meapy.meapy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+/**
+ * Created by yassi on 16/03/2018.
+ */
+
+public class SearchUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
