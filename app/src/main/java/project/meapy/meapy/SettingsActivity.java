643573bb687602e041b333dd15db43211959b553
sansaoui <yassine.sansaoui@gmail.com>
2018-03-23 02:52:38 +0100

    add settings activity to parameter to color theme of the application

---
+++ app/src/main/java/project/meapy/meapy/SettingsActivity.java
@@ -0,0 +1,35 @@
+package project.meapy.meapy;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by yassi on 22/03/2018.
+ */
+
+public class SettingsActivity extends MyAppCompatActivity {
+
+    public static final String KEY_PREFERENCE_KEY = "themeColor";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,new Fragment()).commit();
+
+    }
+
+    public static class Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.activity_settings);
+        }
+    }
+}
