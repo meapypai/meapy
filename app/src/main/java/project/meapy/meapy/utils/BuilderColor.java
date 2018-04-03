4996643b52a29003692a63f99ce69635b0fe88ff
sansaoui <yassine.sansaoui@gmail.com>
2018-04-03 11:45:51 +0200

    modify background chat

---
+++ app/src/main/java/project/meapy/meapy/utils/BuilderColor.java
@@ -0,0 +1,16 @@
+package project.meapy.meapy.utils;
+
+/**
+ * Created by yassi on 03/04/2018.
+ */
+
+public class BuilderColor {
+
+    public static String generateHexaColor() {
+        String color = "#";
+        for(int i = 0; i < 6; i++) {
+            
+        }
+        return color;
+    }
+}
