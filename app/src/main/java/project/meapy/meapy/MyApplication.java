8b1983e35ab9ca95f9aeec8ab554349d704a004d
assimtsenoussi <assim.senoussi@gmail.com>
2018-04-05 14:49:21 +0200

    working to fix user problem

--- app/src/main/java/project/meapy/meapy/MyApplication.java
+++ app/src/main/java/project/meapy/meapy/MyApplication.java
@@ -2,6 +2,9 @@ package project.meapy.meapy;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Map;
 
 import project.meapy.meapy.bean.User;
 
@@ -19,10 +22,43 @@ public class MyApplication extends Application{
     }
 
     public static void setUser(User u) {
+        String lastname = u.getLastName();
+        String chatBubble = u.getChatBubbleColor();
+        String email = u.getEmail();
+        String firstName = u.getFirstName();
+        String imgProfile = u.getNameImageProfil();
+        int rank = u.getRank();
+        String uid = u.getUid();
+        SharedPreferences prefs = c.getSharedPreferences("USER_PREFS",0);
+        prefs.edit().putString("LASTNAME",lastname)
+                .putString("CHAT_BUBBLE",chatBubble)
+                .putString("EMAIL",email)
+                .putString("FIRSTNAME",firstName)
+                .putString("IMG_PROFILE",imgProfile)
+                .putInt("RANK",rank)
+                .putString("UID",uid);
         user = u;
     }
 
     public static User getUser() {
+        if(user == null){
+            SharedPreferences prefs = c.getSharedPreferences("USER_PREFS",0);
+            String lastname = prefs.getString("LASTNAME","");
+            String chatBubble = prefs.getString("CHAT_BUBBLE","");
+            String email = prefs.getString("EMAIL","");
+            String firstName = prefs.getString("FIRSTNAME","");
+            String imgProfile = prefs.getString("IMG_PROFILE","");
+            int rank = prefs.getInt("RANK",0);
+            String uid = prefs.getString("UID","");
+            user = new User();
+            user.setUid(uid);
+            user.setChatBubbleColor(chatBubble);
+            user.setEmail(email);
+            user.setFirstName(firstName);
+            user.setLastName(lastname);
+            user.setNameImageProfil(imgProfile);
+            user.setRank(rank);
+        }
         return user;
     }
 
