0c80f21855c16dfdbf589afd270153f2a04a93aa
assimtsenoussi <assim.senoussi@gmail.com>
2018-04-05 23:13:26 +0200

    adding notif when message receive (not totally working)

--- app/src/main/java/project/meapy/meapy/NotificationThread.java
+++ app/src/main/java/project/meapy/meapy/NotificationThread.java
@@ -1,10 +1,13 @@
 package project.meapy.meapy;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -107,14 +110,35 @@ public class NotificationThread extends Thread {
                         intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
                         PendingIntent pendingIntent = PendingIntent.getActivity(context,REQUEST_NOTIFICATION,intent,PendingIntent.FLAG_ONE_SHOT);
 
-                        Notification.Builder builder = new Notification.Builder(context).setWhen(System.currentTimeMillis())
-                                .setTicker("title1")
-                                .setSmallIcon(R.drawable.logo_app1)
-                                .setContentTitle("title2")
-                                .setContentText(msg.getContent())
-                                .setContentIntent(pendingIntent);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            // Create the NotificationChannel
+                            CharSequence name = "channel_name";
+                            String description = "desc";
+                            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                            NotificationChannel mChannel = new NotificationChannel("default", name, importance);
+                            mChannel.setDescription(description);
+                            // Register the channel with the system; you can't change the importance
+                            // or other notification behaviors after this
+                            NotificationManager notificationManager = (NotificationManager) context.getSystemService(
+                                    NOTIFICATION_SERVICE);
+                            notificationManager.createNotificationChannel(mChannel);
+                            NotificationCompat.Builder buildr = new NotificationCompat.Builder(context, "default")
+                                    .setTicker("title1")
+                                    .setSmallIcon(R.drawable.logo_app1)
+                                    .setContentTitle(msg.getNameUser())
+                                    .setContentText(msg.getContent())
+                                    .setContentIntent(pendingIntent);
 
-                        manager.notify(msg.getId(),builder.build());
+                            notificationManager.notify(10,buildr.build());
+                        }else{
+                            Notification.Builder builder = new Notification.Builder(context).setWhen(System.currentTimeMillis())
+                                    .setTicker("title1")
+                                    .setSmallIcon(R.drawable.logo_app1)
+                                    .setContentTitle(msg.getNameUser())
+                                    .setContentText(msg.getContent())
+                                    .setContentIntent(pendingIntent);
+                            manager.notify(11,builder.build());
+                        }
                     }
                 },null);
             }
