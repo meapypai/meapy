463d9a650ccffac2dc083d94467d509378d86c02
sansaoui <yassine.sansaoui@gmail.com>
2018-04-08 21:56:56 +0200

    comment code wich provoks error in discipline activity

--- app/src/main/java/project/meapy/meapy/AddDisciplineActivity.java
+++ app/src/main/java/project/meapy/meapy/AddDisciplineActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
@@ -21,13 +25,12 @@ import project.meapy.meapy.utils.firebase.DisciplineLink;
 public class AddDisciplineActivity extends AppCompatActivity {
 
     public static final String GROUP_EXTRA_NAME = "GROUP";
-    public static final String DISCIPLINES_EXTRA_NAME = "DISCIPLINES";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_discipline);
         final Groups grp = (Groups) getIntent().getSerializableExtra(GROUP_EXTRA_NAME);
-        final ArrayList<Discipline> disciplines = getIntent().getParcelableArrayListExtra(DISCIPLINES_EXTRA_NAME);
         Button okBtn = findViewById(R.id.okBtnAddDisc);
 
         okBtn.setOnClickListener(new View.OnClickListener() {
@@ -36,17 +39,17 @@ public class AddDisciplineActivity extends AppCompatActivity {
                 EditText edit = findViewById(R.id.disciplineNameAddDisc);
                 String discName = edit.getText().toString();
                 if(discName.length() > 0) {
-                    if(!nameDiscplineAlreadyExists(disciplines,discName)) {
+//                    if(!nameDiscplineAlreadyExists(disciplines,discName)) {
                         Discipline disc = new Discipline();
                         disc.setName(discName);
                         disc.setColor(BuilderColor.generateHexaColor());
                         DisciplineLink.addDiscipline(grp,disc);
                         Toast.makeText(AddDisciplineActivity.this, getString(R.string.discipline_added_toast) + discName, Toast.LENGTH_LONG).show();
                         finish();
-                    }
-                    else {
-                        Toast.makeText(AddDisciplineActivity.this,getResources().getString(R.string.displine_already_exists),Toast.LENGTH_SHORT).show();
-                    }
+//                    }
+//                    else {
+//                        Toast.makeText(AddDisciplineActivity.this,getResources().getString(R.string.displine_already_exists),Toast.LENGTH_SHORT).show();
+//                    }
                 }
             }
         });
