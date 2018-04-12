0222ba5c79c6c08b4de70862b2d390699c7ef7c1
assimtsenoussi <assim.senoussi@gmail.com>
2018-04-12 21:03:53 +0200

    refactoring onegroupe activity

--- app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
+++ app/src/main/java/project/meapy/meapy/groups/OneGroupActivity.java
@@ -84,7 +84,6 @@ public class OneGroupActivity extends MyAppCompatActivity {
 
     private TextView titleGroup;
     private TextView summaryOneGroup;
-    private ImageView accedToDiscussionOneGroup;
 
     RecyclerView listView;
     PostAdapter2 adapterPost;
@@ -96,8 +95,6 @@ public class OneGroupActivity extends MyAppCompatActivity {
 
     private FloatingActionButton fBtn;
 
-    public static final int LEAVE_GROUP_REQUEST = 1;
-
     public static final String GROUP_NAME_EXTRA = "GROUP";
 
     @Override
@@ -108,100 +105,67 @@ public class OneGroupActivity extends MyAppCompatActivity {
         listView = findViewById(R.id.postsOneGroup);
         summaryOneGroup = findViewById(R.id.summaryOneGroup);
         titleGroup = findViewById(R.id.groupNameOneGroup);
-        accedToDiscussionOneGroup = (ImageView)findViewById(R.id.accedToDiscussionOneGroup);
 
         final Groups grp = (Groups) getIntent().getSerializableExtra(GROUP_NAME_EXTRA);
         group = grp;
 
-        adapterPost = new PostAdapter2(posts,group,new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int position = listView.getChildLayoutPosition(view);
-                Post post = (Post) adapterPost.getItem(position);
-                Intent intent = new Intent(OneGroupActivity.this, PostDetailsActivity.class);
-                intent.putExtra(PostDetailsActivity.POST_EXTRA_NAME,post);
-                intent.putExtra(PostDetailsActivity.ID_GROUP_EXTRA_NAME,group.getId()+"");
-                startActivity(intent);
-            }
-        });
-
-        String name = grp.getName();
-        titleGroup.setText(name);
-        summaryOneGroup.setText(grp.getSummary());
-
+        providePost();
+        configureSummaryGroup();
         configureToolbar();
+        configureDiscMenu();
+        configureColorNavigationView();
+        provideDiscipline();
+        configureSendFileAction();
+        configureImageGroup();
+        configureLeaveGroupAction();
+    }
 
-        final Menu menu = ((NavigationView)findViewById(R.id.side_menu_one_group)).getMenu();
-        subMenuDisc = menu.addSubMenu("Discipline");
-        ImageButton addDisc = findViewById(R.id.addDiscOneGroup);
-        addDisc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                askNewDiscName();
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-        final NavigationView navigationView = findViewById(R.id.side_menu_one_group);
+        //change widgets'color in terms of settings
+        fBtn.setBackgroundTintList(ContextCompat.getColorStateList(this,colorId));
+    }
 
-        findViewById(R.id.headerNavigationView).setBackground(getColorDrawable());
-        findViewById(R.id.footerNavigationView).setBackground(getColorDrawable());
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        listView.setAdapter(adapterPost);
-        listView.setLayoutManager(new LinearLayoutManager(this));
-
-        DisciplineLink.getDisciplineByGroupId(group.getId(), new RunnableWithParam() {
-            @Override
-            public void run() {
-                onDisciplineAdded((Discipline) getParam());
-            }
-        }, null);
-
-        //listView.setOnClickListener();
-//
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Post post = (Post) adapterPost.getItem(i);
-//                Intent intent = new Intent(OneGroupActivity.this, PostDetailsActivity.class);
-//                intent.putExtra(PostDetailsActivity.POST_EXTRA_NAME,post);
-//                intent.putExtra(PostDetailsActivity.ID_GROUP_EXTRA_NAME,group.getId()+"");
-//                startActivity(intent);
-//            }
-//        });
-
-        fBtn = findViewById(R.id.sendFileOneGroup);
-        fBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(OneGroupActivity.this, SendFileActivity.class);
-                intent.putExtra(SendFileActivity.GROUP_EXTRA_NAME,grp);
-                startActivity(intent);
-            }
-        });
-
-        /*accedToDiscussionOneGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inviteDiscussionAction();
-            }
-        });
-        */
-
-        ImageView imageView = findViewById(R.id.imageGroupOneGroup);
-
-        //si image par défault
-        if(grp.getImageName().equals(CreateGroupActivity.DEFAULT_IMAGE_GROUP)) {
-            imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.group_default));
+        Intent intent = null ;
+        switch(item.getItemId()) {
+            case R.id.inviteOneGroup:
+                inviteAction();
+                break;
+            case R.id.accedToDiscussionOneGroup:
+                intent = new Intent(OneGroupActivity.this, ChatRoomActivity.class);
+                intent.putExtra(ChatRoomActivity.EXTRA_GROUP_ID,group.getId()+"");
+                intent.putExtra(ChatRoomActivity.EXTRA_GROUP_NAME,group.getName());
+                break;
+            case R.id.membersOneGroup:
+                intent  = new Intent(this, MembersActivity.class);
+                intent.putExtra(ChatRoomActivity.EXTRA_GROUP_ID,group.getId()+"");
+                break;
+            case R.id.shareGroup:
+                intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT,getResources().getString(R.string.invitation_code_to_group) + " " );
+                intent.setType("text/plain");
+                intent = Intent.createChooser(intent,"Share code");
         }
-        else {
-            StorageReference ref = FirebaseStorage.getInstance().getReference("data_groups/" + grp.getId() +"/"+grp.getImageName());
-            Glide.with(getApplicationContext()).using(new FirebaseImageLoader()).load(ref).asBitmap().into(imageView);
-
+        if(intent != null) {
+            startActivity(intent);
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.onegroup_menu, menu);
+        return true;
+    }
+    private void configureLeaveGroupAction(){
         findViewById(R.id.leaveGroupPostDetails).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(getApplicationContext(),"delete post",Toast.LENGTH_LONG).show();
                 Handler mHandler = new Handler(Looper.getMainLooper());
                 mHandler.post(new Runnable() {
                     public void run() {
@@ -211,16 +175,7 @@ public class OneGroupActivity extends MyAppCompatActivity {
                         builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
-                                if(fUser != null) {
-                                    String uid = fUser.getUid();
-                                    GroupLink.leaveGroups(uid,group);
-                                    Toast.makeText(getApplicationContext(), getString(R.string.leave_group_toast), Toast.LENGTH_LONG).show();
-                                    Intent intent = new Intent(getApplicationContext(), MyGroupsActivity.class);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                    startActivity(intent);
-                                    finish();
-                                }
+                                onLeaveGroup();
                             }
                         });
                         AlertDialog dialog = builder.create();
@@ -230,7 +185,18 @@ public class OneGroupActivity extends MyAppCompatActivity {
             }
         });
     }
-
+    private void onLeaveGroup(){
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(fUser != null) {
+            String uid = fUser.getUid();
+            GroupLink.leaveGroups(uid,group);
+            Toast.makeText(getApplicationContext(), getString(R.string.leave_group_toast), Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getApplicationContext(), MyGroupsActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+    }
     private void onCreateDiscipline(final EditText edit){
         String discName = edit.getText().toString();
         if (discName.length() > 0) {
@@ -266,7 +232,6 @@ public class OneGroupActivity extends MyAppCompatActivity {
              public void run() {
                  if(dialogNewDisc == null) {
                      AlertDialog.Builder builder = new AlertDialog.Builder(OneGroupActivity.this);
-
                      LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                              LinearLayout.LayoutParams.MATCH_PARENT);
                      nameEdit.setLayoutParams(lp);
@@ -282,33 +247,93 @@ public class OneGroupActivity extends MyAppCompatActivity {
                      dialogNewDisc.show();
                      dialogNewDisc.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
                  }
-
-                 nameEdit.addTextChangedListener(new TextWatcher() {
-                     @Override
-                     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-                     @Override
-                     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-                     @Override
-                     public void afterTextChanged(Editable editable) {
-                         String discName = nameEdit.getText().toString();
-                         if(discName.length() > 0 && !nameDiscplineAlreadyExists(listDiscipline, discName)) {
-                             dialogNewDisc.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
-                         }else{
-                             dialogNewDisc.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
-                         }
-                     }
-                 });
+                 configureOnTextChangeNewDisc(nameEdit);
              }
          }
         );
     }
+    private void configureOnTextChangeNewDisc(final EditText nameEdit){
+        nameEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String discName = nameEdit.getText().toString();
+                if(discName.length() > 0 && !nameDiscplineAlreadyExists(listDiscipline, discName)) {
+                    dialogNewDisc.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                }else{
+                    dialogNewDisc.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+                }
+            }
+        });
+    }
+    private void providePost(){
+        adapterPost = new PostAdapter2(posts,group,new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = listView.getChildLayoutPosition(view);
+                Post post = (Post) adapterPost.getItem(position);
+                Intent intent = new Intent(OneGroupActivity.this, PostDetailsActivity.class);
+                intent.putExtra(PostDetailsActivity.POST_EXTRA_NAME,post);
+                intent.putExtra(PostDetailsActivity.ID_GROUP_EXTRA_NAME,group.getId()+"");
+                startActivity(intent);
+            }
+        });
+        listView.setAdapter(adapterPost);
+        listView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    private void configureSummaryGroup(){
+        String name = group.getName();
+        titleGroup.setText(name);
+        summaryOneGroup.setText(group.getSummary());
+    }
+    private void configureDiscMenu(){
+        final Menu menu = ((NavigationView)findViewById(R.id.side_menu_one_group)).getMenu();
+        subMenuDisc = menu.addSubMenu("Discipline");
+        ImageButton addDisc = findViewById(R.id.addDiscOneGroup);
+        addDisc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askNewDiscName();
+            }
+        });
+    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private void configureColorNavigationView(){
+        findViewById(R.id.headerNavigationView).setBackground(getColorDrawable());
+        findViewById(R.id.footerNavigationView).setBackground(getColorDrawable());
+    }
+    private void provideDiscipline(){
+        DisciplineLink.getDisciplineByGroupId(group.getId(), new RunnableWithParam() {
+            @Override
+            public void run() {
+                onDisciplineAdded((Discipline) getParam());
+            }
+        }, null);
+    }
+    private void configureSendFileAction(){
+        fBtn = findViewById(R.id.sendFileOneGroup);
+        fBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OneGroupActivity.this, SendFileActivity.class);
+                intent.putExtra(SendFileActivity.GROUP_EXTRA_NAME,group);
+                startActivity(intent);
+            }
+        });
+    }
+    private void configureImageGroup(){
+        ImageView imageView = findViewById(R.id.imageGroupOneGroup);
+        if(group.getImageName().equals(CreateGroupActivity.DEFAULT_IMAGE_GROUP)) {
+            imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.group_default));
+        }
+        else {
+            StorageReference ref = FirebaseStorage.getInstance().getReference("data_groups/" + group.getId() +"/"+group.getImageName());
+            Glide.with(getApplicationContext()).using(new FirebaseImageLoader()).load(ref).asBitmap().into(imageView);
 
-        //change widgets'color in terms of settings
-        fBtn.setBackgroundTintList(ContextCompat.getColorStateList(this,colorId));
+        }
     }
 
     private void configureToolbar(){
@@ -368,41 +393,6 @@ public class OneGroupActivity extends MyAppCompatActivity {
             posts.remove(toDelete);
             adapterPost.notifyDataSetChanged();
         }
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.onegroup_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        Intent intent = null ;
-        switch(item.getItemId()) {
-            case R.id.inviteOneGroup:
-                inviteAction();
-                break;
-            case R.id.accedToDiscussionOneGroup:
-                intent = new Intent(OneGroupActivity.this, ChatRoomActivity.class);
-                intent.putExtra(ChatRoomActivity.EXTRA_GROUP_ID,group.getId()+"");
-                intent.putExtra(ChatRoomActivity.EXTRA_GROUP_NAME,group.getName());
-                break;
-            case R.id.membersOneGroup:
-                intent  = new Intent(this, MembersActivity.class);
-                intent.putExtra(ChatRoomActivity.EXTRA_GROUP_ID,group.getId()+"");
-                break;
-            case R.id.shareGroup:
-                intent = new Intent(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_TEXT,getResources().getString(R.string.invitation_code_to_group) + " " );
-                intent.setType("text/plain");
-                intent = Intent.createChooser(intent,"Share code");
-        }
-        if(intent != null) {
-            startActivity(intent);
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void inviteAction(){
