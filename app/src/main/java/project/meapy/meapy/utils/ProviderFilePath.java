ec000bcde1104e90d93dbef19ce8203f70e6ce2c
sansaoui <yassine.sansaoui@gmail.com>
2018-04-13 12:08:19 +0200

    add strings

--- app/src/main/java/project/meapy/meapy/utils/ProviderFilePath.java
+++ app/src/main/java/project/meapy/meapy/utils/ProviderFilePath.java
@@ -3,8 +3,10 @@ package project.meapy.meapy.utils;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.widget.Toast;
 
 /**
  * Created by yassi on 06/03/2018.
@@ -27,25 +29,32 @@ public class ProviderFilePath {
         String path = "";
         String autority = uri.getAuthority();
 
-        if(autority.equals("com.android.providers.media.documents")) {
-            String[] split = uri.getLastPathSegment().split(":"); //on recupere le dernier segment de l'uri ( image:48414)
-            String type = split[0];
-            String id   = split[1];
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            if (autority.equals("com.android.providers.media.documents")) {
+                String[] split = uri.getLastPathSegment().split(":"); //on recupere le dernier segment de l'uri ( image:48414)
+                String type = split[0];
+                String id = split[1];
 
-            System.out.println(type);
-            System.out.println(id);
-
-            //si le  document est une image
-            if(type.equals("image")) {
-                path = getData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,new String[] {MediaStore.Images.Media.DATA},"_id=?",new String[]{id});
+                //si le  document est une image
+                if (type.equals("image")) {
+                    path = getData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Images.Media.DATA}, "_id=?", new String[]{id});
+                }
             }
+            else if (autority.equals("com.android.providers.downloads.documents")) {
+                String idDoc = DocumentsContract.getDocumentId(uri); //on récupère l'id du document récupéré
+                Uri uri2 = Uri.withAppendedPath(Uri.parse("content://downloads/public_downloads"), idDoc);
+                path = getData(uri2, new String[]{"_data"}, null, null);
+            }
+            else if (autority.equals("com.android.externalstorage.documents")) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                String type = split[0];
+                
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            }
+            return path;
         }
-        else if (autority.equals("com.android.providers.downloads.documents")) {
-            String idDoc = DocumentsContract.getDocumentId(uri); //on récupère l'id du document récupéré
-            Uri uri2 = Uri.withAppendedPath(Uri.parse("content://downloads/public_downloads"),idDoc);
-            path =  getData(uri2,new String[]{"_data"},null,null);
-        }
-        return path;
+        return null;
     }
 
     public String getData(Uri uri,String[] columns, String selection,String[] args) {
